761c26aed480cac8f137d03637dbfb74c08f9893
Nathan L Smith <nsmith@newrelic.com>
2019-02-13 00:21:17 -0600

    StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,16 @@ import Sidebar from "./Sidebar";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Graph
-          dataUrl="http://js.cytoscape.org/demos/cose-layout/data.json"
-          styleUrl="http://js.cytoscape.org/demos/colajs-graph/cy-style.json"
-        >
-          <Sidebar />
-        </Graph>
-      </div>
+      <React.StrictMode>
+        <div className="App">
+          <Graph
+            dataUrl="http://js.cytoscape.org/demos/cose-layout/data.json"
+            styleUrl="http://js.cytoscape.org/demos/colajs-graph/cy-style.json"
+          >
+            <Sidebar />
+          </Graph>
+        </div>
+      </React.StrictMode>
     );
   }
 }
