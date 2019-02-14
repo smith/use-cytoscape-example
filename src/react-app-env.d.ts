5200438d60b09901c3ee24a217499eae23d46b65
Nathan L Smith <nsmith@newrelic.com>
2019-02-14 15:23:25 -0600

    Add type definitions

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,17 @@
 /// <reference types="react-scripts" />
+import * as React from "react";
+import * as cytoscape from "cytoscape";
+
+declare module "cytoscape" {
+  export interface Core {
+    mount(element: ELement): cytoscape.Core;
+  }
+  export default cytoscape;
+}
 
 declare module "scheduler/tracing";
+
+declare module "react" {
+  export const unstable_Profiler: any;
+  export default React;
+}
